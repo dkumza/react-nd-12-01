03242940c64c25b6d4b5ec6936de8a7deacdfb74
Darius <dariuskumza@gmail.com>
2023-12-01 16:49:28 +0200

    counter with button comp.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ import { Task02 } from "./Components/Task02";
 // Paspaudus vieną iš mygtukų žaliame kvadrate atsiranda mažesnis kvadratas su medžio pavadinimu viduje.
 // Svarbu, kad visi medžiai žaliame kvadrate būtų suskirstyti į tris stulpelius pagal medžio pavadinimą.
 
+// #2 Sukurti aplikaciją su vienu mygtuku “pridėti”.
+// Paspaudus mygtuką, atsirastų juodas kvadratas su mygtuku “+” ir skaičiumi 0 viduje.
+// Paspaudus kelis kartus mygtuką pridėti atsirastų keli tokie juodi kvadratai, paspaudus 3 - atsirastų 3 ir t.t.
+// Spaudant mygtuką “+” kiekviename iš juodų kvadratų jų viduje esantis skaičius didėtų vienetu
+// (kiekviename kvadrate atskirai).
+
 function App() {
    return (
       <div className="container d-flex justify-content-center m-5 ">
--- src/Components/Task02/Counter.jsx
+++ src/Components/Task02/Counter.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
+import { Button } from "../Button";
 
 export function Counter() {
    const [score, setScore] = useState(0);
 
+   const handleScore = () => {
+      setScore(score + 1);
+   };
+
    return (
       <div className="square d-flex flex-column justify-content-around align-items-center">
          <h1>{score}</h1>
-         <button
-            className="btn btn-outline-success w-50"
-            onClick={() => setScore(score + 1)}
-         >
-            +
-         </button>
+         <Button
+            handleClick={handleScore}
+            newClass="btn btn-outline-success m-3 w-50"
+            name="+"
+         />
       </div>
    );
 }
